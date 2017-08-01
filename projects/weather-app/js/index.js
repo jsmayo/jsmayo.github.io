7dc3c0dd6ff70cc2b54c1c827bce87063273101f
Steven Mayo <jsmayo91@gmail.com>
2017-08-01 18:40:31 -0400

    Modifies: Celsius typo in index page

--- projects/weather-app/js/index.js
+++ projects/weather-app/js/index.js
@@ -4,7 +4,7 @@ if(navigator.geolocation){
 		var long = position.coords.longitude;
 		$.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat +  "&lon=" + long, function(json) {
 			$("#myLoc").html(json.name + ", " + json.sys.country);
-			$("#myTemp").html(json.main.temp + " Celcius");
+			$("#myTemp").html(json.main.temp + " Celsius");
 			// $("#myConditions").html(json.weather[0].main + ", " + json.weather[0].description);
 			$("#myConditions").html(json.weather[0].description);
 			$("#icon").html("<img src=" + json.weather[0].icon + ">")
