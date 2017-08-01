752ab9397311af7d9c195bcb0f75e45b9edf907e
Steven Mayo <jsmayo91@gmail.com>
2017-08-01 18:38:25 -0400

    Adds: Index.html and Javascript Library

---
+++ projects/weather-app/js/index.js
@@ -0,0 +1,16 @@
+if(navigator.geolocation){
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var lat = position.coords.latitude;
+		var long = position.coords.longitude;
+		$.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat +  "&lon=" + long, function(json) {
+			$("#myLoc").html(json.name + ", " + json.sys.country);
+			$("#myTemp").html(json.main.temp + " Celcius");
+			// $("#myConditions").html(json.weather[0].main + ", " + json.weather[0].description);
+			$("#myConditions").html(json.weather[0].description);
+			$("#icon").html("<img src=" + json.weather[0].icon + ">")
+		});
+		
+	});
+} else {
+	$("#myLoc").html("Cannot Determine Location");
+}
