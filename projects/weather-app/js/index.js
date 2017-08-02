8b56a7b43c9675e178fff61cec9ea09f62ba71ea
Steven Mayo <jsmayo91@gmail.com>
2017-08-02 17:43:59 -0400

    Modifies: 1. Javascript to change icons based on weather response codes from API. 2. CSS file to include animated weather icons. There is also a fallback 	to default to FCC API icons if there is no match found for mine.

--- projects/weather-app/js/index.js
+++ projects/weather-app/js/index.js
@@ -1,16 +1,73 @@
-if(navigator.geolocation){
+if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(function(position) {
 		var lat = position.coords.latitude;
 		var long = position.coords.longitude;
-		$.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat +  "&lon=" + long, function(json) {
-			$("#myLoc").html(json.name + ", " + json.sys.country);
-			$("#myTemp").html(json.main.temp + " Celsius");
-			// $("#myConditions").html(json.weather[0].main + ", " + json.weather[0].description);
-			$("#myConditions").html(json.weather[0].description);
-			$("#icon").html("<img src=" + json.weather[0].icon + ">")
-		});
-		
+		var wCode = "";
+		$.getJSON(
+			"https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long,
+			function(json) {
+				$("#myLoc").html(json.name + ", " + json.sys.country);
+				$("#myTemp").html(json.main.temp + " Celsius");
+				// $("#myConditions").html(json.weather[0].main + ", " + json.weather[0].description);
+				$("#myConditions").html(json.weather[0].description);
+				//get weather condition string to make compairison for custom icons
+				iCond = json.weather[0].id;
+
+				// Fall back on default icons if no match is found
+				if (getWeatherIcon(iCond) === -1) {
+					$("#icon").html("<img src=" + json.weather[0].icon + ">");
+				} else {
+					// Use my included icon pack
+					$("#icon").html(getWeatherIcon(iCond));
+				}
+			}
+		);
 	});
 } else {
 	$("#myLoc").html("Cannot Determine Location");
+}
+
+function getWeatherIcon(wc) {
+	var wCode = parseInt(wc);
+
+	// Thunderstorm
+	if (wCode >= 200 && wCode <= 290) {
+		return "<div class='icon thunder-storm'>\
+  					<div class='cloud'></div>\
+  					<div class='lightning'>\
+   				    	<div class='bolt'></div>\
+    					<div class='bolt'></div>\
+  					</div>\
+				</div>";
+	} else if ((wCode >= 300 && wCode <= 399) || (wCode >= 500 && wCode <= 599)) {
+		// Rain
+		return "<div class='icon rainy'>\
+  					<div class='cloud'></div>\
+  					<div class='rain'></div>\
+				</div>";
+	} else if (wCode >= 600 && wCode <= 699) {
+		// Snow
+		return '<div class="icon flurries">\
+				    <div class="cloud"></div>\
+				    <div class="snow">\
+				        <div class="flake"></div>\
+				        <div class="flake"></div>\
+				  </div>\
+				</div>';
+	} else if (wCode === 800) {
+		// Clear
+		return '<div class="icon sunny">\
+				    <div class="sun">\
+				   	     <div class="rays"></div>\
+				    </div>\
+				</div>';
+	} else if (wCode > 800 && wCode < 810) {
+		// Cloudy
+		return '<div class="icon cloudy">\
+				    <div class="cloud"></div>\
+				    <div class="cloud"></div>\
+				</div>';
+	} else
+		// Fall back on default icon
+		return -1;
 }
