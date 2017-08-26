51c3400b5c7dcc445f5f6236d419fccca5606306
Steven Mayo <jsmayo91@gmail.com>
2017-08-26 19:39:07 -0400

    Modifies: javascript of TTT game.

--- projects/tictactoe/js/js.js
+++ projects/tictactoe/js/js.js
@@ -15,11 +15,17 @@ window.onload = function() {
 	filled = [false,false,false,false,false,false,false,false,false];
 	symbol = ['','','','','','','','',''];
 	winner = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-
+// new game button
 	var n = document.getElementById("new");
 	n.addEventListener("click", newGame);
-
+	//remember, adding ()s will cause the function to run, this is a REFERENCE.
 	function newGame() {
+		// built JS in refresh call 
 		document.location.reload();
 	}
+	//Event listener that gets the ID of the box that was clicked.
+	document.getElementById("tic").addEventListener("click", function(e) {
+		// e = Event created by the event listener, target = The item that caused the event to trigger (CANVAS), id = the HTML id of the object.
+		boxClick(e.target.id);
+	});
 }
