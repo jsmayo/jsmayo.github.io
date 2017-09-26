171b183fbd5d5a6ede5cbc677bd826f066a8d33d
Steven Mayo <jsmayo91@gmail.com>
2017-09-26 01:31:33 -0400

    Adds: Skeleton directory and source files for Tick-Tock.  *  Working on this during my spare time, in-between exams/projects         etc.  * Currently adding script.js with a displayTime method, which creates 	a new Date variable to pull data from the Hours/Minutes/Seconds 	attributes and display it to the user.

---
+++ projects/tick-tock/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  function displayTime() {
+    var currentTime = new Date(); // create a date var that's in-sync with the local pc clock.
+    var hour = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  } 
+  
+  
+  
+});
